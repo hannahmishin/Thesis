89bfbe0faefba09849d4405c738927cf851db042
HANNAH_MISHIN <hannah.mishin@gmail.com>
2014-02-13 16:10:57 -0500

    changes in logic after talking with Surya

--- Thesis_logic_solving/Thesis_logic_solving.ino
+++ Thesis_logic_solving/Thesis_logic_solving.ino
@@ -11,7 +11,9 @@ Stepper steppers0(stepsofstepper, 2, 3, 4, 5);
 Stepper steppers1(stepsofstepper, 6, 7, 8, 9);
 Stepper steppers2(stepsofstepper, 10, 11, 12, 13);
 
-Stepper StepperMotors[3]= {steppers0, steppers1, steppers2};
+///arraySize should match at all times the actual amount of the stepperMotor array
+int arraySize = 3;
+Stepper stepperMotors[3]= {steppers0, steppers1, steppers2};
 
 
 
@@ -19,19 +21,23 @@ Stepper StepperMotors[3]= {steppers0, steppers1, steppers2};
 
 #define NUM_SENSORS             3  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  3  // average 4 analog samples per sensor reading
-QTRSensorsAnalog qtra((unsigned char[]) {0}, 
+QTRSensorsAnalog qtra((unsigned char[]) {
+  0}
+, 
 NUM_SENSORS, NUM_SAMPLES_PER_SENSOR);
 
 unsigned int sensorValues[NUM_SENSORS];
 
 unsigned int IRValue[0];
- 
+
 int IRthreshold = 800;
 
 ///set logic stuff for master slave stepper stuff
-boolean Master = false;
 
-byte MasterDefined;
+boolean input;
+
+int currentMasterIndex;
+
 
 void setup(){
   Serial.begin(9600);
@@ -39,25 +45,42 @@ void setup(){
 }
 
 void loop(){
-  if (Master == false);
-  ///execute neutral behavior movement
-  if (Master == true);
-  ///set all steppers to slave stepper array here
- Stepper Slaves[3]= StepperMotors[3];
- 
- ///does not work. 
- Stepper Slaves[MasterDefined] = Stepper MasterStepper;
-  
-}
 
+  if(input){
+      for(int i=0; i< arraySize; i++){
+
+      if(i == currentMasterIndex){
+        //Fear(currentMasterIndex);
+        // do master behaviour for this index
+      }
+      else{
+        //this index is not a master so execute slave behaviour
+
+      }
+      input = false;
+    
+  else{
+        for(int i=0; i< arraySize; i++){
+        //no people so all steppers set to neutral
+        }
+
+  }
+  }
+  }
+
+}
 void serialEvent(){
   while (Serial.available()){
-    MasterDefined = (byte)Serial.read();
-  ///finish up the handshaking
-  
+    currentMasterIndex = (byte)Serial.read();
+    ///finish up the handshaking
+    thereIsInput = true;
+    
+    //Clear current stepper behavious
+
   }
 }
-    
+
+
 
 
 
