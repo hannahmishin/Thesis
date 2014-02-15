dfcbc904f4eb72b63b6ed3c42f764b202a9f95a9
HANNAH_MISHIN <hannah.mishin@gmail.com>
2014-02-15 11:29:46 -0500

    serial Comm  Problem Solving

--- serialcontrol/serialcontrol.ino
+++ serialcontrol/serialcontrol.ino
@@ -10,7 +10,8 @@ AccelStepper steppers2(AccelStepper::FULL4WIRE, 10, 11, 12, 13);
 
 ///arraySize should match at all times the actual amount of the stepperMotor array
 int arraySize = 3;
-AccelStepper stepperMotors[3]= {steppers0, steppers1, steppers2};
+AccelStepper stepperMotors[3]= {
+  steppers0, steppers1, steppers2};
 
 
 //IR STUFF
@@ -30,58 +31,65 @@ int IRthreshold = 800;
 
 ///set logic stuff for master slave stepper stuff
 
-boolean input;
+int input = 0;
+int j=0; //temp int for looping through and stopping test step run
 
-int currentMasterIndex = 0;
+int currentMasterIndex;
+int inByte;
 
 
 void setup(){
   Serial.begin(9600);
-    for (int i=0; i< arraySize; i ++){
-  stepperMotors[i].setMaxSpeed(1000);
-  stepperMotors[i].setSpeed(50);
+  for (int i=0; i< arraySize; i ++){
+    stepperMotors[i].setMaxSpeed(1000);
+    stepperMotors[i].setSpeed(1000);
   }
-
-
 
 }
 
 void loop(){
+  Serial.println(j);
   //hannah look at logic here!!! later where do the if statements live etc.
-  if(currentMasterIndex>0 && input == true){
-  //  for(int i=0; i< arraySize; i++){
-       stepperMotors[1].runSpeed();
-        delay (10);
-        stepperMotors[2].runSpeed();   
-    }
-     // if(i == currentMasterIndex){
-       // do master behaviour for this index
-        //
-      }
-//      else{
-//        //this index is not a master so execute slave behaviour
-// input = false;
-//      }
-      //else{
-     //   for(int i=0; i< arraySize; i++){
-          //no people so all steppers set to neutral
-          
-      //  }
-  //  }
-//  }
+  if(input == 1){
+    if(j < 350){
 
-//}
+      stepperMotors[currentMasterIndex].runSpeed();
+     // stepperMotors[1].runSpeed();
+      //stepperMotors[2].runSpeed();
+      j++;
+      if (j > 349){
+        input = 0; 
+        j=0;
+        Serial.println("stop motors");
+      }   
+    } 
+  }
+  delay(1);
+}
+
 void serialEvent(){
   while (Serial.available()){
-    currentMasterIndex = Serial.read();
+   inByte= Serial.read();
     ///finish up the handshaking
-    input = true;
-    delay (10);
-    Serial.print(input);
-}
-    //Clear current stepper behavious
+    input = 1;
+    switch (inByte){
+      case '1':
+      currentMasterIndex=0;
+      break;
+      case '2':
+      currentMasterIndex= 1;
+      break;
+      case '3': 
+      currentMasterIndex = 2;
+    } 
+    Serial.println("currentMasterIndex");
+    Serial.println( currentMasterIndex);
+delay (30);
   }
-//}
+}
+
+
+
 
 
 
